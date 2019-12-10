2f8d7e14e6df856a6e73f8a7430b85e60052b6e9
Your Name <you@example.com>
2019-12-10 00:45:01 +0000

    vwaps

--- deribit-vwap.py
+++ deribit-vwap.py
@@ -67,13 +67,13 @@ LOG_LEVEL           = logging.INFO
 MIN_ORDER_SIZE      = 1
 MAX_LAYERS          =  3        # max orders to layer the ob with on each side
 MKT_IMPACT          =  0      # base 1-sided spread between bid/offer
-NLAGS               =  2        # number of lags in time series
+NLAGS               =  3        # number of lags in time series
 PCT                 = 100 * BP  # one percentage point
 PCT_LIM_LONG        = 800       # % position limit long
 PCT_LIM_SHORT       = 1600       # % position limit short
 PCT_QTY_BASE        = 1000       # pct order qty in bps as pct of acct on each order
 MIN_LOOP_TIME       =   0.1       # Minimum time between loops
-RISK_CHARGE_VOL     =   4   # vol risk charge in bps per 100 vol
+RISK_CHARGE_VOL     =   7.5   # vol risk charge in bps per 100 vol
 SECONDS_IN_DAY      = 3600 * 24
 SECONDS_IN_YEAR     = 365 * SECONDS_IN_DAY
 WAVELEN_MTIME_CHK   = 15        # time in seconds between check for file change
@@ -591,9 +591,9 @@ class MarketMaker( object ):
                     coin = 'BTC/USD'
                 if i == 'deribit':
                     coin = 'BTC-PERPETUAL'
-                ohlcv = clients[i].fetchOHLCV(coin, '30m')
+                ohlcv = clients[i].fetchOHLCV(coin, '1m')
                 if i == 'deribit':
-                    ohlcv = requests.get('https://www.deribit.com/api/v2/public/get_tradingview_chart_data?instrument_name=BTC-PERPETUAL&start_timestamp=' + str(int(time.time()) * 1000 - 1000 * 60 * 200) + '&end_timestamp=' + str(int(time.time())* 1000) + '&resolution=30')
+                    ohlcv = requests.get('https://www.deribit.com/api/v2/public/get_tradingview_chart_data?instrument_name=BTC-PERPETUAL&start_timestamp=' + str(int(time.time()) * 1000 - 1000 * 60 * 200) + '&end_timestamp=' + str(int(time.time())* 1000) + '&resolution=1')
                     j = ohlcv.json()
                     o = []
                     h = []
